fee24f545a23ebb438b2f5df0e8f0aa7a4094dda
Ming Jun <87565927+mongj@users.noreply.github.com>
2026-05-21 00:54:15 +0800

    Add: assign-cookies - submission-0

---
+++ Data Structures & Algorithms/assign-cookies/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        i = j = 0
+        contentCount = 0
+        while i < len(g) and j < len(s):
+            while j < len(s) and s[j] < g[i]:
+                j += 1
+
+            if j == len(s):
+                break
+
+            contentCount += 1
+            i += 1
+            j += 1
+            
+        return contentCount
