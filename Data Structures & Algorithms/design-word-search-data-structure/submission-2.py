60417d209f8b025c9e0bb6470fa563857de172ee
Ming Jun <87565927+mongj@users.noreply.github.com>
2026-08-04 00:09:11 +0800

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,37 @@
+class TrieNode:
+
+    def __init__(self, val):
+        self.val = val
+        self.is_end = False
+        self.children = {}
+
+class WordDictionary:
+
+    def __init__(self):
+        self.trie = TrieNode(None)
+
+    def addWord(self, word: str) -> None:
+        current_node = self.trie
+        for c in word:
+            if c not in current_node.children:
+                current_node.children[c] = TrieNode(c)
+            current_node = current_node.children[c]
+        current_node.is_end = True
+
+    def searchFrom(self, word: str, node: TrieNode) -> bool:
+        current_node = node
+        for i in range(len(word)):
+            if word[i] == ".":
+                for node in current_node.children.values():
+                    if self.searchFrom(word[i + 1:], node):
+                        return True
+                return False
+            elif word[i] in current_node.children:
+                current_node = current_node.children[word[i]]
+            else:
+                return False
+        return current_node.is_end
+    
+    def search(self, word: str) -> bool:
+        return self.searchFrom(word, self.trie)
+
