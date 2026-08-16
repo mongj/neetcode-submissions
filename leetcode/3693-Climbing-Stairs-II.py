7f34ed791c74417489c6a81274ed714927bef5d7
Ming Jun <87565927+mongj@users.noreply.github.com>
2026-08-16 18:08:09 +0800

    [3693] [Time Beats: 23.52%] [Memory Beats: 11.19%] - LeetPush

--- leetcode/3693-Climbing-Stairs-II.py
+++ leetcode/3693-Climbing-Stairs-II.py
@@ -1,11 +1,14 @@
 class Solution:
     def climbStairs(self, n: int, costs: List[int]) -> int:
+        cache = {}
         def sub(n: int) -> int:
             if n <= 0:
                 return 0
-            return min(
-                (sub(n - 1) + costs[n-1] + 1),
-                (sub(n - 2) + costs[n-1] + 4),
-                (sub(n - 3) + costs[n-1] + 9)
-            )
+            if n not in cache:
+                cache[n] = min(
+                    (sub(n - 1) + costs[n-1] + 1),
+                    (sub(n - 2) + costs[n-1] + 4),
+                    (sub(n - 3) + costs[n-1] + 9)
+                )
+            return cache[n]
         return sub(n)
