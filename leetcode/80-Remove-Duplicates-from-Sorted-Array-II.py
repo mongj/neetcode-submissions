ef5a3fca9a06f4b299abdc25b110d415e6717650
Ming Jun <87565927+mongj@users.noreply.github.com>
2026-08-21 22:17:14 +0800

    [80] [Time Beats: 34.68%] [Memory Beats: 9.03%] - LeetPush

--- leetcode/80-Remove-Duplicates-from-Sorted-Array-II.py
+++ leetcode/80-Remove-Duplicates-from-Sorted-Array-II.py
@@ -1,16 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        def shouldAdd(i: int, k: int) -> bool:
-            if i == 0 or i == 1 or nums[i] != nums[k-1]:
-                return True
-            else:
-                return nums[i] != nums[k-2]
-            
         k = 0
         for i in range(len(nums)):
-            if shouldAdd(i, k):
-                nums[k] = nums[i]
-                k += 1
+            if i > 1 and nums[i] == nums[k - 1] == nums[k - 2]:
+                continue
+            nums[k] = nums[i]
+            k += 1
         return k
 
         
