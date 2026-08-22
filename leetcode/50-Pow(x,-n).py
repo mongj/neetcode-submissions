0b763a9e477283fa896577f969ac23fbd257cdef
Ming Jun <87565927+mongj@users.noreply.github.com>
2026-08-22 15:53:24 +0800

    [50] [Time Beats: 100.00%] [Memory Beats: 86.74%] - LeetPush

---
+++ leetcode/50-Pow(x,-n).py
@@ -0,0 +1,13 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+
+        if n % 2 == 0:
+            sub = self.myPow(x, n / 2)
+            return sub * sub
+        else:
+            return x * self.myPow(x, n - 1)
