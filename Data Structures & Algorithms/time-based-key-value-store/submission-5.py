21bdd34670a9078edeaf8cfbeb2a89e0bc180509
Ming Jun <87565927+mongj@users.noreply.github.com>
2026-09-06 12:46:58 +0800

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,22 @@
+class TimeMap:
+
+    def __init__(self):
+        self.mp = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.mp:
+            self.mp[key] = []
+        self.mp[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        values = self.mp.get(key, [])
+        l, r = 0, len(values) - 1
+        bestSeen = ""
+        while l <= r:
+            m = l + (r - l) // 2
+            if values[m][1] > timestamp:
+                r = m - 1
+            else:
+                bestSeen = values[m][0]
+                l = m + 1
+        return bestSeen
