50784c6bff7d357acf418382040816f981326f7a
Ming Jun <87565927+mongj@users.noreply.github.com>
2026-09-11 19:15:06 +0800

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereqMap = defaultdict(list)
+        for p in prerequisites:
+            prereqMap[p[0]].append(p[1])
+        
+        visited = set()
+        def hasCycle(course_id: int, path: dict) -> bool:
+            if course_id in path:
+                return True
+            if course_id in visited:
+                return False
+            path[course_id] = "dummy"
+            visited.add(course_id)
+
+            for prereq in prereqMap[course_id]:
+                if hasCycle(prereq, path):
+                    return True
+
+            path.pop(course_id)
+            return False
+        
+        path = {}
+        for course in range(numCourses):
+            if hasCycle(course, path):
+                return False
+        return True
